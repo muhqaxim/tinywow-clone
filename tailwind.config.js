9e245af636e2219bf498e07e1005366db4db91c9
MuhQasimBhatti <muhammadqasimbhatti4@gmail.com>
2024-07-31 13:00:00 +0500

    Umer's code added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
+    screens: {
+      "c-md": "576px",
+    },
     extend: {
-      backgroundImage: theme => ({
-        'custom-gradient': 'linear-gradient(87.56deg, #1A8FE3 1.79%, rgba(26, 143, 227, 0) 383.1%)',
-      }),
+      screens: {
+        "c-md": "576px",
+      },
     },
   },
   plugins: [],
-}
-
+};
